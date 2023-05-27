94b94a318b395121e1edcb9bd4d5df8834b95859
Brent Lasaca <brent.lasaca@cit.edu>
2023-05-27 23:17:32 +0800

    Delete assessment question

--- performance-assessment-application/src/App.js
+++ performance-assessment-application/src/App.js
@@ -1,20 +1,18 @@
 import { Stack } from '@mui/material';
 import './App.css';
 import AssessmentTitle from './Create Assessment/Components/AssessmentTitle';
-import AssessmentQuestion from './Create Assessment/Components/AssessmentQuestion';
 
 function App() {
   return (
     <div
       style={{
-        minHeight: "100vh", // Set the minimum height of the container to 100% of viewport height
+        minHeight: "100vh",
         width: "100vw",
         backgroundColor: "#d6f4f8",
       }}
     >
       <Stack direction="column" spacing={2}>
         <AssessmentTitle />
-        <AssessmentQuestion />
       </Stack>
     </div>
   );
