81459d38d941859632b3589e004b35929d66813e
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-05-30 21:05:05 +0800

    Add ChoiceCreationDto

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/ChoiceCreationDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PerformanceAssessmentApi.Dtos
+{
+    public class ChoiceCreationDto
+    {
+        [Required(ErrorMessage = "The choiceValue is required.")]
+        public string? ChoiceValue { get; set; }
+
+        [Required(ErrorMessage = "The itemId is required.")]
+        public int ItemId { get; set; }
+    }
+}
