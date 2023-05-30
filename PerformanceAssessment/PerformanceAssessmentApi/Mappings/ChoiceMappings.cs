f85d80bb524a1f04057e218fd5d7dd258845eb9c
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-05-30 21:06:44 +0800

    Add ChoiceMappings

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Mappings/ChoiceMappings.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Mappings
+{
+    public class ChoiceMappings : Profile
+    {
+        public ChoiceMappings()
+        {
+            CreateMap<ChoiceCreationDto, Choice>(MemberList.None)
+                .ForMember(dto => dto.ChoiceValue, opt => opt.MapFrom(st => (st.ChoiceValue!)))
+                .ForMember(dto => dto.ItemId, opt => opt.MapFrom(st => (st.ItemId!)));
+
+            CreateMap<ChoiceUpdationDto, Choice>(MemberList.None)
+                .ForMember(dto => dto.ChoiceValue, opt => opt.MapFrom(st => (st.ChoiceValue!)));
+        }
+    }
+}
