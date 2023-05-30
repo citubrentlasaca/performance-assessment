24773ee98112ab3a71d11fbb1ecafe2aeafaa4a9
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-05-30 21:04:40 +0800

    Add AssessmentDto

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/AssessmentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PerformanceAssessmentApi.Dtos
+{
+    public class AssessmentDto
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
