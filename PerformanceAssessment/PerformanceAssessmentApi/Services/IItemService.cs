6ca0862a6d2819acd0cca47f96f8544ed791e3b8
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-05-30 21:09:37 +0800

    Add IItemService

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Services/IItemService.cs
@@ -0,0 +1,18 @@
+﻿using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Services
+{
+    public interface IItemService
+    {
+        Task<Item> CreateItem(ItemCreationDto item);
+
+        Task<IEnumerable<ItemChoiceDto>> GetAllItems();
+
+        Task<ItemChoiceDto> GetItemById(int id);
+
+        Task<int> UpdateItem(int id, ItemUpdationDto item);
+
+        Task<int> DeleteItem(int id);
+    }
+}
