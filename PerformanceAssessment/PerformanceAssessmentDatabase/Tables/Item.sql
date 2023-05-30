49822e3a3775d6b5a84beae70e657cf377818d31
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-05-30 21:01:25 +0800

    Add Item Table

---
+++ PerformanceAssessment/PerformanceAssessmentDatabase/Tables/Item.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Item]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Question] NVARCHAR(MAX) NOT NULL, 
+    [QuestionType] NVARCHAR(MAX) NOT NULL,
+    [Weight] FLOAT NULL, 
+    [Required] BIT NOT NULL,
+    [AssessmentId] INT NOT NULL,
+    CONSTRAINT [FK_ItemAssessment] FOREIGN KEY ([AssessmentId]) REFERENCES [Assessment]([Id]) ON DELETE CASCADE
+)
