610cc186c1b96e6dab8e1aa8f4f1746449aca516
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-25 23:20:15 +0800

    Remove Weight Field in ChoiceCreationDto

--- PerformanceAssessment/PerformanceAssessmentApi/Dtos/ChoiceCreationDto.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/ChoiceCreationDto.cs
@@ -7,9 +7,6 @@ namespace PerformanceAssessmentApi.Dtos
         [Required(ErrorMessage = "The choiceValue is required.")]
         public string? ChoiceValue { get; set; }
 
-        [Required(ErrorMessage = "The weight is required.")]
-        public float Weight { get; set; }
-
         [Required(ErrorMessage = "The itemId is required.")]
         public int ItemId { get; set; }
     }
