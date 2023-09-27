00857018db9599f6e57b6de61f7994b9be49296f
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-27 15:30:20 +0800

    Add SelfAssessmentChoiceDto

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/SelfAssessmentChoiceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PerformanceAssessmentApi.Dtos
+{
+    public class SelfAssessmentChoiceDto
+    {
+        public int Id { get; set; }
+        public string? ChoiceValue { get; set; }
+        public int SelfAssessmentItemId { get; set; }
+    }
+}
