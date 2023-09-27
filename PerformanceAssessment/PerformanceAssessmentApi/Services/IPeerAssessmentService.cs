bf92cdbb8f173ec62763c1588fc866e321aea73f
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-27 20:45:04 +0800

    Create IPeerAssessmentService

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Services/IPeerAssessmentService.cs
@@ -0,0 +1,20 @@
+﻿using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Services
+{
+    public interface IPeerAssessmentService
+    {
+        Task<PeerAssessment> CreatePeerAssessment(PeerAssessmentCreationDto assessment);
+
+        Task<IEnumerable<PeerAssessmentDto>> GetAllPeerAssessments();
+
+        Task<PeerAssessmentDto> GetPeerAssessmentById(int id);
+
+        Task<int> UpdatePeerAssessment(int id, PeerAssessmentUpdationDto assessment);
+
+        Task<int> DeletePeerAssessment(int id);
+
+        Task<PeerAssessmentItemsDto?> GetPeerAssessmentItemsById(int id);
+    }
+}
