adb07470ff159a72a0b20cd47b79f0a1ecaa162c
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-27 15:32:13 +0800

    Add SelfAssessmentItemUpdationDto

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/SelfAssessmentItemUpdationDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PerformanceAssessmentApi.Dtos
+{
+    public class SelfAssessmentItemUpdationDto
+    {
+        [Required(ErrorMessage = "The question is required.")]
+        public string? Question { get; set; }
+
+        [Required(ErrorMessage = "The questionType is required.")]
+        public string? QuestionType { get; set; }
+
+        [Required(ErrorMessage = "The weight is required.")]
+        public float Weight { get; set; }
+
+        [Required(ErrorMessage = "The target is required.")]
+        public float Target { get; set; }
+
+        [Required(ErrorMessage = "The required field is required.")]
+        public bool Required { get; set; }
+    }
+}
