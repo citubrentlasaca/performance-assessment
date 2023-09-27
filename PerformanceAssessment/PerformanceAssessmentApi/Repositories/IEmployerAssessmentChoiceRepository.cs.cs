01c0b6b831b2105d1e28fa402e32dcd744ed85f8
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-28 00:19:23 +0800

    Add IEmployerAssessmentChoiceRepository

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Repositories/IEmployerAssessmentChoiceRepository.cs.cs
@@ -0,0 +1,18 @@
+﻿using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Repositories
+{
+    public interface IEmployerAssessmentChoiceRepository
+    {
+        Task<int> CreateEmployerAssessmentChoice(EmployerAssessmentChoice choice);
+
+        Task<IEnumerable<EmployerAssessmentChoiceDto>> GetAllEmployerAssessmentChoices();
+
+        Task<EmployerAssessmentChoiceDto> GetEmployerAssessmentChoiceById(int id);
+
+        Task<int> UpdateEmployerAssessmentChoice(EmployerAssessmentChoice choice);
+
+        Task<int> DeleteEmployerAssessmentChoice(int id);
+    }
+}
