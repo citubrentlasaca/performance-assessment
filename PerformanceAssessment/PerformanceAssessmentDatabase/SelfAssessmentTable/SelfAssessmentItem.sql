100896fe2f75210dee483727bf91619a7b99618e
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-27 15:28:50 +0800

    Add SelfAssessmentItem Table

---
+++ PerformanceAssessment/PerformanceAssessmentDatabase/SelfAssessmentTable/SelfAssessmentItem.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SelfAssessmentItem]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Question] NVARCHAR(MAX) NOT NULL, 
+    [QuestionType] NVARCHAR(MAX) NOT NULL,
+    [Weight] FLOAT NOT NULL,
+    [Target] FLOAT NOT NULL, 
+    [Required] BIT NOT NULL,
+    [SelfAssessmentId] INT NOT NULL,
+    CONSTRAINT [FK_SelfAssessmentItemSelfAssessment] FOREIGN KEY ([SelfAssessmentId]) REFERENCES [SelfAssessment]([Id]) ON DELETE CASCADE
+)
