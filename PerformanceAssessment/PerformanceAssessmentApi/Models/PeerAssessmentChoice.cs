e74caf6405d744da6c9f7180eecb8d4ecb1d4f83
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-27 20:38:43 +0800

    Add PeerAssessmentChoice Model

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Models/PeerAssessmentChoice.cs
@@ -0,0 +1,9 @@
+﻿namespace PerformanceAssessmentApi.Models
+{
+    public class PeerAssessmentChoice
+    {
+        public int Id { get; set; }
+        public string? ChoiceValue { get; set; }
+        public int PeerAssessmentItemId { get; set; }
+    }
+}
