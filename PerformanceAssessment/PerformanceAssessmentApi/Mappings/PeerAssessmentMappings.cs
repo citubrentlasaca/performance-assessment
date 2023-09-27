52da73add97287592c9cc89feebff12639ce4c98
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-27 20:42:09 +0800

    Implement PeerAssessmentMappings

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Mappings/PeerAssessmentMappings.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Mappings
+{
+    public class PeerAssessmentMappings : Profile
+    {
+        public PeerAssessmentMappings()
+        {
+            CreateMap<PeerAssessmentCreationDto, PeerAssessment>(MemberList.None)
+                .ForMember(dto => dto.Title, opt => opt.MapFrom(st => (st.Title!)))
+                .ForMember(dto => dto.Description, opt => opt.MapFrom(st => (st.Description!)));
+
+            CreateMap<PeerAssessmentUpdationDto, PeerAssessment>(MemberList.None)
+                .ForMember(dto => dto.Title, opt => opt.MapFrom(st => (st.Title!)))
+                .ForMember(dto => dto.Description, opt => opt.MapFrom(st => (st.Description!)));
+        }
+    }
+}
