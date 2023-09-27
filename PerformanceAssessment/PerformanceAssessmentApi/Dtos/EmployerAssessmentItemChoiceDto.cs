e739471cdb1b0e7826a6729e2010ea0e5f0de4a3
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-28 00:14:45 +0800

    Create EmployerAssessmentItemChoiceDto

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/EmployerAssessmentItemChoiceDto.cs
@@ -0,0 +1,14 @@
+﻿namespace PerformanceAssessmentApi.Dtos
+{
+    public class EmployerAssessmentItemChoiceDto
+    {
+        public int Id { get; set; }
+        public string? Question { get; set; }
+        public string? QuestionType { get; set; }
+        public float Weight { get; set; }
+        public float Target { get; set; }
+        public bool Required { get; set; }
+        public int EmployerAssessmentId { get; set; }
+        public List<EmployerAssessmentChoiceDto> EmployerAssessmentChoices { get; set; } = new List<EmployerAssessmentChoiceDto>();
+    }
+}
