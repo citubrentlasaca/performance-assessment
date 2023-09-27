8d58a6d77ae3df2aa15120d7cd1f10b8f8b9d72c
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-28 00:16:23 +0800

    Implement EmployerAssessmentMappings

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Mappings/EmployerAssessmentMappings.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Mappings
+{
+    public class EmployerAssessmentMappings : Profile
+    {
+        public EmployerAssessmentMappings()
+        {
+            CreateMap<EmployerAssessmentCreationDto, EmployerAssessment>(MemberList.None)
+                .ForMember(dto => dto.Title, opt => opt.MapFrom(st => (st.Title!)))
+                .ForMember(dto => dto.Description, opt => opt.MapFrom(st => (st.Description!)));
+
+            CreateMap<EmployerAssessmentUpdationDto, EmployerAssessment>(MemberList.None)
+                .ForMember(dto => dto.Title, opt => opt.MapFrom(st => (st.Title!)))
+                .ForMember(dto => dto.Description, opt => opt.MapFrom(st => (st.Description!)));
+        }
+    }
+}
