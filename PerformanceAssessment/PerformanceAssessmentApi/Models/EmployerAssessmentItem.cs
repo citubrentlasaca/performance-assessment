7b7ef03d0b5692af11c58559e6c70ae1d2229195
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-28 00:12:40 +0800

    Add EmployerAssessmentItem Model

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Models/EmployerAssessmentItem.cs
@@ -0,0 +1,13 @@
+﻿namespace PerformanceAssessmentApi.Models
+{
+    public class EmployerAssessmentItem
+    {
+        public int Id { get; set; }
+        public string? Question { get; set; }
+        public string? QuestionType { get; set; }
+        public float Weight { get; set; }
+        public float Target { get; set; }
+        public bool Required { get; set; }
+        public int EmployerAssessmentId { get; set; }
+    }
+}
