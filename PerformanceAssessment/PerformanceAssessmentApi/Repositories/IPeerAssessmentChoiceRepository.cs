53bb910353153017b8f80feda69717e67212dca4
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-27 20:44:02 +0800

    Add IPeerAssessmentChoiceRepository

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Repositories/IPeerAssessmentChoiceRepository.cs
@@ -0,0 +1,18 @@
+﻿using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Repositories
+{
+    public interface IPeerAssessmentChoiceRepository
+    {
+        Task<int> CreatePeerAssessmentChoice(PeerAssessmentChoice choice);
+
+        Task<IEnumerable<PeerAssessmentChoiceDto>> GetAllPeerAssessmentChoices();
+
+        Task<PeerAssessmentChoiceDto> GetPeerAssessmentChoiceById(int id);
+
+        Task<int> UpdatePeerAssessmentChoice(PeerAssessmentChoice choice);
+
+        Task<int> DeletePeerAssessmentChoice(int id);
+    }
+}
