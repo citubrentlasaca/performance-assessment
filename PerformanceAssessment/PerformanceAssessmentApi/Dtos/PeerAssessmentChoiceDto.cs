ae6644ee97a4ea1279d4d23aea371bf48bc19888
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-27 20:39:32 +0800

    Create PeerAssessmentChoiceDto

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/PeerAssessmentChoiceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PerformanceAssessmentApi.Dtos
+{
+    public class PeerAssessmentChoiceDto
+    {
+        public int Id { get; set; }
+        public string? ChoiceValue { get; set; }
+        public int PeerAssessmentItemId { get; set; }
+    }
+}
