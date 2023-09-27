61a5361aca07143d3737e0f9905d74dbdaa69f92
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-27 15:30:34 +0800

    Add SelfAssessmentChoiceUpdationDto

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/SelfAssessmentChoiceUpdationDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PerformanceAssessmentApi.Dtos
+{
+    public class SelfAssessmentChoiceUpdationDto
+    {
+        [Required(ErrorMessage = "The choiceValue is required.")]
+        public string? ChoiceValue { get; set; }
+    }
+}
