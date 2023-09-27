df1d1a1a9970fe5632899f1e9abf1fdddc8d654c
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-27 15:34:33 +0800

    Create ISelfAssessmentRepository

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Repositories/ISelfAssessmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Repositories
+{
+    public interface ISelfAssessmentRepository
+    {
+        Task<int> CreateSelfAssessment(SelfAssessment assessment);
+
+        Task<IEnumerable<SelfAssessmentDto>> GetAllSelfAssessments();
+
+        Task<SelfAssessmentDto> GetSelfAssessmentById(int id);
+
+        Task<int> UpdateSelfAssessment(SelfAssessment assessment);
+
+        Task<int> DeleteSelfAssessment(int id);
+
+        Task<SelfAssessmentItemsDto?> GetSelfAssessmentItemsById(int id);
+    }
+}
