662b4e5c2a94072252965b551f498c66ec7fff0a
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-28 14:15:56 +0800

    Add Scripts

---
+++ PerformanceAssessment/PerformanceAssessmentDatabase/Scripts/PopulatePeerAssessmentChoiceData.sql
@@ -0,0 +1,16 @@
+﻿USE [PerformanceAssessmentDatabase]
+GO
+
+SET IDENTITY_INSERT [dbo].[PeerAssessmentChoice] ON
+
+INSERT INTO [dbo].[PeerAssessmentChoice] ([Id], [ChoiceValue], [PeerAssessmentItemId])
+VALUES
+    (1, 'Yes', 3),
+    (2, 'No', 3),
+    (5, '1', 4),
+    (6, '2', 4),
+    (7, '3', 4),
+    (8, '4', 4),
+    (9, '5', 4);
+
+SET IDENTITY_INSERT [dbo].[PeerAssessmentChoice] OFF
---
+++ PerformanceAssessment/PerformanceAssessmentDatabase/Scripts/PopulateSelfAssessmentChoiceData.sql
@@ -0,0 +1,18 @@
+﻿USE [PerformanceAssessmentDatabase]
+GO
+
+SET IDENTITY_INSERT [dbo].[SelfAssessmentChoice] ON
+
+INSERT INTO [dbo].[SelfAssessmentChoice] ([Id], [ChoiceValue], [SelfAssessmentItemId])
+VALUES
+    (1, 'Exceeded expectations', 2),
+    (2, 'Met expectations', 2),
+    (3, 'Partially met expectations', 2),
+    (4, 'Did not meet expectations', 2),
+
+    (5, 'Very effective', 4),
+    (6, 'Effective', 4),
+    (7, 'Somewhat effective', 4),
+    (8, 'Not effective', 4);
+
+SET IDENTITY_INSERT [dbo].[SelfAssessmentChoice] OFF
