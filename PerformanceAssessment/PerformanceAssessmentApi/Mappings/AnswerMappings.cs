8f498de1ae9d847d14f43a821fc10bcae37f390a
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-30 16:43:07 +0800

    Add AnswerMappings

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Mappings/AnswerMappings.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Mappings
+{
+    public class AnswerMappings : Profile
+    {
+        public AnswerMappings()
+        {
+            CreateMap<AnswerCreationDto, Answer>(MemberList.None)
+                .ForMember(dto => dto.AssessmentId, opt => opt.MapFrom(st => (st.AssessmentId!)))
+                .ForMember(dto => dto.ItemId, opt => opt.MapFrom(st => (st.ItemId!)))
+                .ForMember(dto => dto.AnswerText, opt => opt.MapFrom(st => (st.AnswerText!)))
+                .ForMember(dto => dto.SelectedChoices, opt => opt.MapFrom(st => (st.SelectedChoices!)))
+                .ForMember(dto => dto.CounterValue, opt => opt.MapFrom(st => (st.CounterValue!)));
+
+            CreateMap<AnswerUpdationDto, Answer>(MemberList.None)
+                .ForMember(dto => dto.AssessmentId, opt => opt.MapFrom(st => (st.AssessmentId!)))
+                .ForMember(dto => dto.ItemId, opt => opt.MapFrom(st => (st.ItemId!)))
+                .ForMember(dto => dto.AnswerText, opt => opt.MapFrom(st => (st.AnswerText!)))
+                .ForMember(dto => dto.SelectedChoices, opt => opt.MapFrom(st => (st.SelectedChoices!)))
+                .ForMember(dto => dto.CounterValue, opt => opt.MapFrom(st => (st.CounterValue!)));
+        }
+    }
+}
