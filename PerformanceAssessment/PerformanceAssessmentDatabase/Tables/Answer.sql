dad07149115373fb013d04a26101fa83a2bcb101
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-09-30 16:36:56 +0800

    Add Answer Table

---
+++ PerformanceAssessment/PerformanceAssessmentDatabase/Tables/Answer.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Answer]
+(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [AssessmentId] INT NOT NULL,
+    [ItemId] INT NOT NULL,
+    [AnswerText] NVARCHAR(MAX) NULL,
+    [SelectedChoices] NVARCHAR(MAX) NULL,
+    [CounterValue] FLOAT NULL,
+    CONSTRAINT [FK_AnswerAssessment] FOREIGN KEY ([AssessmentId]) REFERENCES [Assessment]([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_AnswerItem] FOREIGN KEY ([ItemId]) REFERENCES [Item]([Id]) ON DELETE CASCADE
+)
