5610e982cd53c5f10cb483efc47ffece3e77952e
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-03 01:39:00 +0800

    Update AnswerRepository With Necessary Changes

--- PerformanceAssessment/PerformanceAssessmentApi/Repositories/AnswerRepository.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Repositories/AnswerRepository.cs
@@ -16,8 +16,9 @@ namespace PerformanceAssessmentApi.Repositories
 
         public async Task<int> SaveAnswers(Answer answer)
         {
-            var sql = "INSERT INTO [dbo].[Answer] ([AssessmentId], [ItemId], [AnswerText], [SelectedChoices], [CounterValue]) " +
-                      "VALUES (@AssessmentId, @ItemId, @AnswerText, @SelectedChoices, @CounterValue); " +
+            var sql = "INSERT INTO [dbo].[Answer] " +
+                      "([ItemId], [AnswerText], [SelectedChoices], [CounterValue], [IsDeleted], [DateTimeAnswered]) " +
+                      "VALUES (@ItemId, @AnswerText, @SelectedChoices, @CounterValue, 0, @DateTimeAnswered); " +
                       "SELECT SCOPE_IDENTITY();";
 
             using (var con = _context.CreateConnection())
@@ -26,19 +27,9 @@ namespace PerformanceAssessmentApi.Repositories
             }
         }
 
-        public async Task<IEnumerable<AnswerDto>> GetAnswersByAssessmentId(int assessmentId)
-        {
-            var sql = "SELECT * FROM [dbo].[Answer] WHERE [AssessmentId] = @AssessmentId;";
-
-            using (var con = _context.CreateConnection())
-            {
-                return await con.QueryAsync<AnswerDto>(sql, new { AssessmentId = assessmentId });
-            }
-        }
-
         public async Task<IEnumerable<AnswerDto>> GetAnswersByItemId(int itemId)
         {
-            var sql = "SELECT * FROM [dbo].[Answer] WHERE [ItemId] = @ItemId;";
+            var sql = "SELECT * FROM [dbo].[Answer] WHERE [ItemId] = @ItemId AND [IsDeleted] = 0;";
 
             using (var con = _context.CreateConnection())
             {
@@ -48,7 +39,7 @@ namespace PerformanceAssessmentApi.Repositories
 
         public async Task<AnswerDto> GetAnswersById(int id)
         {
-            var sql = "SELECT * FROM [dbo].[Answer] WHERE [Id] = @Id;";
+            var sql = "SELECT * FROM [dbo].[Answer] WHERE [Id] = @Id AND [IsDeleted] = 0;";
 
             using (var con = _context.CreateConnection())
             {
@@ -59,7 +50,6 @@ namespace PerformanceAssessmentApi.Repositories
         public async Task<int> UpdateAnswers(Answer answer)
         {
             var sql = "UPDATE [dbo].[Answer] SET " +
-                      "[AssessmentId] = @AssessmentId, " +
                       "[ItemId] = @ItemId, " +
                       "[AnswerText] = @AnswerText, " +
                       "[SelectedChoices] = @SelectedChoices, " +
@@ -68,24 +58,13 @@ namespace PerformanceAssessmentApi.Repositories
 
             using (var con = _context.CreateConnection())
             {
-                return await con.ExecuteScalarAsync<int>(
-                    sql,
-                    new
-                    {
-                        Id = answer.Id,
-                        AssessmentId = answer.AssessmentId,
-                        ItemId = answer.ItemId,
-                        AnswerText = answer.AnswerText,
-                        SelectedChoices = answer.SelectedChoices,
-                        CounterValue = answer.CounterValue
-                    }
-                );
+                return await con.ExecuteAsync(sql, answer);
             }
         }
 
         public async Task<int> DeleteAnswers(int id)
         {
-            var sql = "DELETE FROM [dbo].[Answer] WHERE [Id] = @Id;";
+            var sql = "UPDATE [dbo].[Answer] SET [IsDeleted] = 1 WHERE [Id] = @Id;";
 
             using (var con = _context.CreateConnection())
             {
