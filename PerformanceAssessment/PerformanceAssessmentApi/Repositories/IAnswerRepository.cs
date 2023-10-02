bac1c9d95d1c0e74f8c204b69583e22de45e4bce
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-03 01:38:17 +0800

    Remove GetAnswersByAssessmentId Method in IAnswerRepository

--- PerformanceAssessment/PerformanceAssessmentApi/Repositories/IAnswerRepository.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Repositories/IAnswerRepository.cs
@@ -6,7 +6,6 @@ namespace PerformanceAssessmentApi.Repositories
     public interface IAnswerRepository
     {
         Task<int> SaveAnswers(Answer answer);
-        Task<IEnumerable<AnswerDto>> GetAnswersByAssessmentId(int assessmentId);
         Task<IEnumerable<AnswerDto>> GetAnswersByItemId(int itemId);
         Task<AnswerDto> GetAnswersById(int id);
         Task<int> UpdateAnswers(Answer answer);
