a89afc2869c1041a08d57d4e2a779cb0f53f261c
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-03 01:35:24 +0800

    Modify Answer Model With Necessary Fields

--- PerformanceAssessment/PerformanceAssessmentApi/Models/Answer.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Models/Answer.cs
@@ -2,11 +2,12 @@
 {
     public class Answer
     {
-        public int Id { get; set; }            
-        public int AssessmentId { get; set; }
+        public int Id { get; set; }        
         public int ItemId { get; set; }
         public string? AnswerText { get; set; }
         public string? SelectedChoices { get; set; }
         public float? CounterValue { get; set; }
+        public bool IsDeleted { get; set; }
+        public string? DateTimeAnswered { get; set; }
     }
 }
