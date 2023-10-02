1e5468c0edc49ceeca1018f1365d90f412f46888
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2023-10-02 21:05:05 +0800

    Create Login page

---
+++ performance-assessment-application/src/Login/Login.js
@@ -0,0 +1,19 @@
+// Login.js
+
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="login-container">
+
+          <p>Assess and track</p>
+          <p>employee performance</p>
+          <p>with ease.</p>
+        
+
+
+    </div>
+  );
+}
+
+export default Login;
