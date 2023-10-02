bb93162208a373109f188d087af410a10f30710b
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2023-10-02 23:51:09 +0800

    Add logo. label. textfield and button

--- performance-assessment-application/src/Login/Login.js
+++ performance-assessment-application/src/Login/Login.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './Login.css'; 
 import loginpic from "../Images/loginpic.png";
+import logo from "../Images/WorkPA-logo.png";
 
 function Login() {
   return (
     <div className="login-container">
       <div className="left-section">
-        <img src={loginpic} alt="Login Pic" />
+        <img src={loginpic} alt="Login Pic" draggable="false" />
           <div className="description">
             <p>Assess and track</p>
             <p>employee performance</p>
-            <p>with ease.</p>
+            <p>with <span className="darkblue-text">ease.</span></p>
           </div>
         </div>
 
+      <div className="right-section">
+        <img className="logo" src={logo}alt="Logo" />
+        <label>Email</label>
+        <input type="email" id="email" />
+        <label>Password</label>
+        <input type="password" id="password" />
+        <button>Login</button>
+      </div>
     </div>
   );
 }
