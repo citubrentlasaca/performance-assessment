ed67a9df4a1d2b21cc5d980a9ebf90d9baf15681
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-03 01:24:54 +0800

    Delete Employer Assessment Models

--- PerformanceAssessment/PerformanceAssessmentApi/Models/EmployerAssessmentChoice.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PerformanceAssessmentApi.Models
-{
-    public class EmployerAssessmentChoice
-    {
-        public int Id { get; set; }
-        public string? ChoiceValue { get; set; }
-        public int EmployerAssessmentItemId { get; set; }
-    }
-}
