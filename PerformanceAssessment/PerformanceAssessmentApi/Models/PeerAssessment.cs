0ac22c504c67076a0f299605c6be191c9e83f945
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-03 01:24:28 +0800

    Delete Peer Assessment Models

--- PerformanceAssessment/PerformanceAssessmentApi/Models/PeerAssessment.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PerformanceAssessmentApi.Models
-{
-    public class PeerAssessment
-    {
-        public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? Description { get; set; }
-    }
-}
--- PerformanceAssessment/PerformanceAssessmentApi/Models/PeerAssessmentChoice.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PerformanceAssessmentApi.Models
-{
-    public class PeerAssessmentChoice
-    {
-        public int Id { get; set; }
-        public string? ChoiceValue { get; set; }
-        public int PeerAssessmentItemId { get; set; }
-    }
-}
