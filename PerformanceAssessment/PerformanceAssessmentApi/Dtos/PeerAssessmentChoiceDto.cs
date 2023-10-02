dd7799e96b93b00b1313dff6644c3128474937c5
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-03 01:26:43 +0800

    Delete Peer Assessment Dtos

--- PerformanceAssessment/PerformanceAssessmentApi/Dtos/PeerAssessmentChoiceDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PerformanceAssessmentApi.Dtos
-{
-    public class PeerAssessmentChoiceDto
-    {
-        public int Id { get; set; }
-        public string? ChoiceValue { get; set; }
-        public int PeerAssessmentItemId { get; set; }
-    }
-}
