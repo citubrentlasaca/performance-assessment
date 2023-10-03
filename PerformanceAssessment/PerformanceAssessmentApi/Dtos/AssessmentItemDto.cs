ccb8a84a2a56f45854b13cb1419fb474c09ab6d7
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-03 11:53:54 +0800

    Add DateTimeCreated and DateTimeUpdated Fields in AssessmentItemDto

--- PerformanceAssessment/PerformanceAssessmentApi/Dtos/AssessmentItemDto.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/AssessmentItemDto.cs
@@ -6,5 +6,7 @@
         public string? Title { get; set; }
         public string? Description { get; set; }
         public List<ItemDto> Items { get; set; } = new List<ItemDto>();
+        public string? DateTimeCreated { get; set; }
+        public string? DateTimeUpdated { get; set; }
     }
 }
