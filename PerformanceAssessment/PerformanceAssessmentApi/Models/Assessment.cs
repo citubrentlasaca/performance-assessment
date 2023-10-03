645ebd7d1d4d51c91a624d0a8f9482d95a12fab8
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-03 11:52:48 +0800

    Add DateTimeCreated and DateTimeUpdated Fields in Assessment Model

--- PerformanceAssessment/PerformanceAssessmentApi/Models/Assessment.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Models/Assessment.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
+        public string? DateTimeCreated { get; set; }
+        public string? DateTimeUpdated { get; set; }
     }
 }
