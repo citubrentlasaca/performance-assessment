008c3d08431255145be804730418d0916f1f1430
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2023-10-04 13:29:34 +0800

    Add import for css file

---
+++ performance-assessment-application/src/Team Creation/SuccessPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SuccessPage.css';
+
+function SuccessPage() {
+  return (
+    <div className="success-page">
+      
+    </div>
+  );
+}
+
+export default SuccessPage;
