3e2afa546bdf19298babc1082883b534a1ae260e
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-04 22:39:13 +0800

    Create IEmployeeService

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Services/IEmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Services
+{
+    public interface IEmployeeService
+    {
+        Task<Employee> CreateEmployee(EmployeeCreationDto employee);
+        Task<IEnumerable<EmployeeDto>> GetAllEmployees();
+        Task<EmployeeDto> GetEmployeeById(int id);
+        Task<EmployeeDto> GetEmployeeByUserId(int userId);
+        Task<EmployeeDto> GetEmployeeByTeamId(int teamId);
+        Task<int> UpdateEmployee(int id, EmployeeUpdationDto employee);
+        Task<int> DeleteEmployee(int id);
+        Task<EmployeeDetailsDto> GetEmployeeDetailsById(int id);
+    }
+}
