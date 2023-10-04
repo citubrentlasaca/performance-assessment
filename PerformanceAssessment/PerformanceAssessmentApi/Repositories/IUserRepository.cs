f43149312f26578996222782266aca6f546d4263
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-04 22:37:01 +0800

    Implement IUserRepository

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<int> CreateUser(User user);
+        Task<IEnumerable<UserDto>> GetAllUsers();
+        Task<UserDto> GetUserById(int id);
+        Task<int> UpdateUser(User user);
+        Task<int> DeleteUser(int id);
+    }
+}
