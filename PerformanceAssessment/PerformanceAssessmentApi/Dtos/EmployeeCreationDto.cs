09a73f2f3ef493dc093ce10d9b1eff09d89716b7
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-04 22:35:14 +0800

    Implement EmployeeDtos

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/EmployeeCreationDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PerformanceAssessmentApi.Dtos
+{
+    public class EmployeeCreationDto
+    {
+        [Required(ErrorMessage = "The userId is required.")]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "The teamId is required.")]
+        public int TeamId { get; set; }
+
+        [Required(ErrorMessage = "The status is required.")]
+        public string? Status { get; set; }
+    }
+}
---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/EmployeeDetailsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PerformanceAssessmentApi.Dtos
+{
+    public class EmployeeDetailsDto
+    {
+        public int Id { get; set; }
+        public List<UserDto> Users { get; set; } = new List<UserDto>();
+        public List<TeamDto> Teams { get; set; } = new List<TeamDto>();
+        public string? Status { get; set; }
+        public string? DateTimeJoined { get; set; }
+    }
+}
