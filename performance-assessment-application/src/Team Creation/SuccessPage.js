cbc5b61fb817a39ab0f8dcbe163c1c1542fe0147
Stephanie Micah Cahutay <stephaniemicah.cahutay@cit.edu>
2023-10-04 13:47:48 +0800

    Add container for message

--- performance-assessment-application/src/Team Creation/SuccessPage.js
+++ performance-assessment-application/src/Team Creation/SuccessPage.js
@@ -4,6 +4,12 @@ import './SuccessPage.css';
 function SuccessPage() {
   return (
     <div className="success-page">
+      <div className="container">
+        <div className="message">
+            <h1>Your team has been successfully created.</h1><br />
+            Thank you for choosing <span className="darkblue-text">WorkPA</span>
+        </div>
+      </div>
       
     </div>
   );
