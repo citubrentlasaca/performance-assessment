ab4ebdeed04652e6285627594625858c7168b211
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-04 22:33:20 +0800

    Create Team Model

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Models/Team.cs
@@ -0,0 +1,15 @@
+﻿namespace PerformanceAssessmentApi.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string? Organization { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? BusinessType { get; set; }
+        public string? BusinessAddress { get; set; }
+        public Guid TeamCode { get; set; }
+        public string? DateTimeCreated { get; set; }
+        public string? DateTimeUpdated { get; set; }
+    }
+}
