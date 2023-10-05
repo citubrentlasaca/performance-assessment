b8fd87586385b531316dc15f211ce25ce9e4cff9
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-05 21:02:13 +0800

    Add AssignSchedulerDetailsDto

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/AssignSchedulerDetailsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PerformanceAssessmentApi.Dtos
+{
+    public class AssignSchedulerDetailsDto
+    {
+        public List<int> EmployeeIds { get; set; }
+        public AssignSchedulerCreationDto Scheduler { get; set; }
+    }
+}
