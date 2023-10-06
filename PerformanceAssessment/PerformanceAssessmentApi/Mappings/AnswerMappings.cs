80ad91943b7402a0854a81aab73aae223065e60b
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-06 14:11:38 +0800

    Add EmployeeId Field in AnswerMappings

--- PerformanceAssessment/PerformanceAssessmentApi/Mappings/AnswerMappings.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Mappings/AnswerMappings.cs
@@ -10,6 +10,7 @@ namespace PerformanceAssessmentApi.Mappings
         public AnswerMappings()
         {
             CreateMap<AnswerCreationDto, Answer>(MemberList.None)
+                .ForMember(dto => dto.EmployeeId, opt => opt.MapFrom(st => (st.EmployeeId!)))
                 .ForMember(dto => dto.ItemId, opt => opt.MapFrom(st => (st.ItemId!)))
                 .ForMember(dto => dto.AnswerText, opt => opt.MapFrom(st => (st.AnswerText!)))
                 .ForMember(dto => dto.SelectedChoices, opt => opt.MapFrom(st => (st.SelectedChoices!)))
@@ -17,6 +18,7 @@ namespace PerformanceAssessmentApi.Mappings
                 .ForMember(dto => dto.DateTimeAnswered, opt => opt.MapFrom(st => (StringUtil.GetCurrentDateTime())));
 
             CreateMap<AnswerUpdationDto, Answer>(MemberList.None)
+                .ForMember(dto => dto.EmployeeId, opt => opt.MapFrom(st => (st.EmployeeId!)))
                 .ForMember(dto => dto.ItemId, opt => opt.MapFrom(st => (st.ItemId!)))
                 .ForMember(dto => dto.AnswerText, opt => opt.MapFrom(st => (st.AnswerText!)))
                 .ForMember(dto => dto.SelectedChoices, opt => opt.MapFrom(st => (st.SelectedChoices!)))
