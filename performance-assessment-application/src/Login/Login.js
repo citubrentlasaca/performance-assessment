786b38c89555a48cd5e85e745c9beadfac907052
Brent Lasaca <brent.lasaca@cit.edu>
2023-10-06 16:57:25 +0800

    General frontend changes

--- performance-assessment-application/src/Login/Login.js
+++ performance-assessment-application/src/Login/Login.js
@@ -2,38 +2,58 @@ import React from 'react';
 import './Login.css';
 import loginpic from "../Images/loginpic.png";
 import logo from "../Images/WorkPA-logo.png";
+import { Stack } from '@mui/material';
 
 function Login() {
   return (
     <div className="login-container">
       <div className="login-content">
-      <div className="login-image-container">
-        <img src={loginpic} alt="Login Pic" draggable="false" />
-        <div className="description">
-          <p>Assess and track</p>
-          <p>employee performance</p>
-          <p>with <span className="darkblue-text">ease.</span></p>
+        <div className="login-image-container">
+          <img src={loginpic} alt="Login Pic" draggable="false" />
+          <div className="description">
+            <p>Assess and track</p>
+            <p>employee performance</p>
+            <p>with <span className="darkblue-text">ease.</span></p>
+          </div>
         </div>
-      </div>
 
-      <div className="login-form-container">
-        <form>
-          <div className="login-form-group">
-            <img className="logo" src={logo} alt="Logo" draggable="false" />
-          </div>
-          <div className="login-form-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" required/>
-          </div>
-          <div className="login-form-group">
-            <label  htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" required/>
-          </div>
-          <div className="login-button-container">
-            <button className="login-button" type="login">Login</button>
-          </div>
-        </form>
-      </div>
+        <div className="login-form-container gap-5">
+          <img src={logo} alt="Work PA Logo" />
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="flex-start"
+            spacing={2}
+            sx={{
+              width: "100%",
+            }}
+          >
+            <b>Email</b>
+            <input type='text' className='form-control'
+              style={{
+                border: '2px solid #aee5ed',
+                borderRadius: '10px'
+              }}
+            />
+            <b>Password</b>
+            <input type='text' className='form-control'
+              style={{
+                border: '2px solid #aee5ed',
+                borderRadius: '10px'
+              }}
+            />
+          </Stack>
+          <button type='button'
+            style={{
+              background: 'linear-gradient(to right, #0076fe, #00c5ff)',
+              border: 'none',
+              borderRadius: '10px',
+              width: '100%',
+              height: '40px',
+              color: 'white'
+            }}
+          >Login</button>
+        </div>
       </div>
     </div>
   );
--- performance-assessment-application/src/Shared/TopBar.js
+++ performance-assessment-application/src/Shared/TopBar.js
@@ -42,26 +42,6 @@ function TopBar() {
                     direction="row"
                     justifyContent="space-center"
                     alignItems="center"
-                    spacing={2}
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                    </svg>
-                    <Stack
-                        direction="column"
-                        justifyContent="center"
-                        alignItems="flex-start"
-                    >
-                        <b>John Doe</b>
-                        <p class='m-0'>johndoe@gmail.com</p>
-                    </Stack>
-                </Stack>
-
-                <Stack
-                    direction="row"
-                    justifyContent="space-center"
-                    alignItems="center"
                     spacing={4}
                 >
                     <Stack
@@ -86,6 +66,16 @@ function TopBar() {
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
                             <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                        </svg>
+                    </Stack>
+                    <Stack
+                        direction="row"
+                        justifyContent="space-center"
+                        alignItems="center"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                         </svg>
                     </Stack>
                 </Stack>
