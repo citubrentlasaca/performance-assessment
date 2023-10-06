a5d2a251d32e45899eb6e5d2a752c9fe659fda48
Brent Lasaca <brent.lasaca@cit.edu>
2023-10-07 00:17:56 +0800

    Remake side bar

--- performance-assessment-application/src/Login/Login.js
+++ performance-assessment-application/src/Login/Login.js
@@ -9,53 +9,60 @@ function Login() {
     <div className="login-container">
       <div className="login-content">
         <div className="login-image-container">
-          <img src={loginpic} alt="Login Pic" draggable="false" />
+          <img src={loginpic} alt="Login Pic" />
           <div className="description">
             <p>Assess and track</p>
             <p>employee performance</p>
             <p>with <span className="darkblue-text">ease.</span></p>
           </div>
         </div>
-
-        <div className="login-form-container gap-5">
-          <img src={logo} alt="Work PA Logo" />
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          spacing={8}
+          sx={{
+            width: '50%',
+            height: '100%',
+          }}
+        >
+          <img src={logo} alt="WorkPA Logo" />
           <Stack
             direction="column"
             justifyContent="center"
             alignItems="flex-start"
             spacing={2}
             sx={{
-              width: "75%",
+              width: '75%',
             }}
           >
             <b>Email</b>
             <input type='text' className='form-control'
               style={{
-                border: '2px solid #aee5ed',
-                borderRadius: '10px'
+                width: '100%'
               }}
             />
             <b>Password</b>
             <input type='text' className='form-control'
               style={{
-                border: '2px solid #aee5ed',
-                borderRadius: '10px',
+                width: '100%'
               }}
             />
           </Stack>
           <button type='button'
             style={{
-              background: 'linear-gradient(to right, #0076fe, #00c5ff)',
-              border: 'none',
-              borderRadius: '10px',
               width: '100px',
-              height: '40px',
+              height: '38px',
+              backgroundImage: 'linear-gradient(to right, #0273ff , #00c6ff)',
+              border: 'none',
+              borderRadius: '6px',
               color: 'white'
             }}
           >Login</button>
-        </div>
+        </Stack>
+
       </div>
-    </div>
+    </div >
   );
 }
 
