65347edb265c92a52edf5ae93de5553f84344707
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-07 13:17:12 +0800

    Add EmployeeTeamInfoDto

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/EmployeeTeamInfoDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PerformanceAssessmentApi.Dtos
+{
+    public class EmployeeTeamInfoDto
+    {
+        [Required(ErrorMessage = "The userId is required.")]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "The teamCode is required.")]
+        public Guid TeamCode { get; set; }
+
+        public string Status = "Active";
+    }
+}
