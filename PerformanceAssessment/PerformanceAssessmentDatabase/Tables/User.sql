d30a503d302fc70194a8e7dd6cb1ccbaf210cfd7
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-08 17:07:10 +0800

    Add Salt Field in User Table

--- PerformanceAssessment/PerformanceAssessmentDatabase/Tables/User.sql
+++ PerformanceAssessment/PerformanceAssessmentDatabase/Tables/User.sql
@@ -5,6 +5,7 @@
     [LastName] NVARCHAR(MAX) NOT NULL,
     [EmailAddress] NVARCHAR(MAX) COLLATE Latin1_General_CS_AS NOT NULL,
     [Password] NVARCHAR(MAX) COLLATE Latin1_General_CS_AS NOT NULL,
+    [Salt] NVARCHAR(MAX) COLLATE Latin1_General_CS_AS NOT NULL,
     [DateTimeCreated] DATETIME NOT NULL,
     [DateTimeUpdated] DATETIME NOT NULL
 )
