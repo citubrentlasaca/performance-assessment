cc8e91b1dd7ec3d0ef702bb1e9acbaf716ba6554
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-13 09:15:13 +0800

    Add New Methods in IEmployeeRepository

--- PerformanceAssessment/PerformanceAssessmentApi/Repositories/IEmployeeRepository.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Repositories/IEmployeeRepository.cs
@@ -5,7 +5,9 @@ namespace PerformanceAssessmentApi.Repositories
 {
     public interface IEmployeeRepository
     {
+        Task<EmployeeDto> GetEmployeeByUserIdAndTeamId(int userId, int teamId);
         Task<int> CreateEmployee(Employee employee);
+        Task<EmployeeDto> GetEmployeeByUserIdAndTeamCode(int userId, Guid teamCode);
         Task<int> CreateEmployeeWithTeamCode(EmployeeTeamInfoDto employee);
         Task<IEnumerable<EmployeeDto>> GetAllEmployees();
         Task<EmployeeDto> GetEmployeeById(int id);
