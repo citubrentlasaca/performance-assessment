16c87d579c20e2b8a028657b31a1c9c473a99fac
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-15 00:37:54 +0800

    Implement ITokenService

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Services/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Services
+{
+    public interface ITokenService
+    {
+        string CreateToken(User user);
+        RefreshToken GenerateRefreshToken();
+        string VerifyToken(string refreshToken, User user);
+    }
+}
