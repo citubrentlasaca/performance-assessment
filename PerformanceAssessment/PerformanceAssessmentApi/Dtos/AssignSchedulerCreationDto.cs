8cc3b6ae61c1856f72486775718c8a41995bb1eb
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-16 23:01:04 +0800

    Modify AssignSchedulerCreationDto

--- PerformanceAssessment/PerformanceAssessmentApi/Dtos/AssignSchedulerCreationDto.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/AssignSchedulerCreationDto.cs
@@ -7,18 +7,10 @@ namespace PerformanceAssessmentApi.Dtos
         [Required(ErrorMessage = "The assessmentId is required.")]
         public int AssessmentId { get; set; }
 
-        [Required(ErrorMessage = "The reminder is required.")]
-        public string? Reminder { get; set; }
-
-        [Required(ErrorMessage = "The occurrence is required.")]
-        public string? Occurrence { get; set; }
-
         [Required(ErrorMessage = "The dueDate is required.")]
         public string? DueDate { get; set; }
 
         [Required(ErrorMessage = "The time is required.")]
         public string? Time { get; set; }
-
-        public float Score { get; set; }
     }
 }
