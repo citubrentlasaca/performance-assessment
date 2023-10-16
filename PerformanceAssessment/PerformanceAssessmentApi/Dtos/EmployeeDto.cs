d2dd42743a9327b62b69a48904a80b460fad12a4
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-16 22:59:12 +0800

    Add Role Field in EmployeeDto

--- PerformanceAssessment/PerformanceAssessmentApi/Dtos/EmployeeDto.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/EmployeeDto.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public int UserId { get; set; }
         public int TeamId { get; set; }
+        public string? Role { get; set; }
         public string? Status { get; set; }
         public string? DateTimeJoined { get; set; }
     }
