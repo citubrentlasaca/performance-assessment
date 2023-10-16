2d99b35df75240cd11b5679deba9e04b140ebea0
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-16 23:04:38 +0800

    Add ResultMappings

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Mappings/ResultMappings.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+using PerformanceAssessmentApi.Utils;
+
+namespace PerformanceAssessmentApi.Mappings
+{
+    public class ResultMappings : Profile
+    {
+        public ResultMappings()
+        {
+            CreateMap<ResultCreationDto, Result>(MemberList.None)
+                .ForMember(dto => dto.AssessmentId, opt => opt.MapFrom(st => (st.AssessmentId!)))
+                .ForMember(dto => dto.EmployeeId, opt => opt.MapFrom(st => (st.EmployeeId!)))
+                .ForMember(dto => dto.DateTimeDue, opt => opt.MapFrom(st => (st.DateTimeDue!)))
+                .ForMember(dto => dto.DateTimeCreated, opt => opt.MapFrom(st => StringUtil.GetCurrentDateTime()))
+                .ForMember(dto => dto.DateTimeUpdated, opt => opt.MapFrom(st => StringUtil.GetCurrentDateTime()));
+
+            CreateMap<ResultUpdationDto, Result>(MemberList.None)
+                .ForMember(dto => dto.AssessmentId, opt => opt.MapFrom(st => (st.AssessmentId!)))
+                .ForMember(dto => dto.EmployeeId, opt => opt.MapFrom(st => (st.EmployeeId!)))
+                .ForMember(dto => dto.Score, opt => opt.MapFrom(st => (st.Score!)))
+                .ForMember(dto => dto.DateTimeDue, opt => opt.MapFrom(st => (st.DateTimeDue!)))
+                .ForMember(dto => dto.DateTimeUpdated, opt => opt.MapFrom(st => StringUtil.GetCurrentDateTime()));
+        }
+    }
+}
