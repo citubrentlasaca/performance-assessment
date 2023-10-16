8f79848cfbb7fb67153f46aa46c571d54d2afc57
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-16 23:09:33 +0800

    Add IResultRepository

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Repositories/IResultRepository.cs
@@ -0,0 +1,16 @@
+﻿using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApi.Repositories
+{
+    public interface IResultRepository
+    {
+        Task<int> CreateResult(Result result);
+        Task<IEnumerable<ResultDto>> GetAllResults();
+        Task<ResultDto> GetResultById(int id);
+        Task<IEnumerable<ResultDto>> GetResultByAssessmentId(int assessmentId);
+        Task<IEnumerable<ResultDto>> GetResultByEmployeeId(int employeeId);
+        Task<int> UpdateResult(Result result);
+        Task<int> DeleteResult(int id);
+    }
+}
