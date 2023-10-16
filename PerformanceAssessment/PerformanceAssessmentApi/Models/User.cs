9d251200a599ddcaf5525ef322154bf76b6b08eb
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-16 22:55:31 +0800

    Modify User Model

--- PerformanceAssessment/PerformanceAssessmentApi/Models/User.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Models/User.cs
@@ -5,13 +5,9 @@
         public int Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public string? Role { get; set; }
         public string? EmailAddress { get; set; }
         public string? Password { get; set; }
         public string? Salt { get; set; }
-        public string RefreshToken { get; set; } = string.Empty;
-        public DateTime TokenCreated { get; set; }
-        public DateTime TokenExpires { get; set; }
         public string? DateTimeCreated { get; set; }
         public string? DateTimeUpdated { get; set; }
     }
