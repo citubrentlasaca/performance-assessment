2e93d320ae33f726277c1e25ceff23b598b24e7d
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-16 23:04:23 +0800

    Modify AssignSchedulerMappings

--- PerformanceAssessment/PerformanceAssessmentApi/Mappings/AssignSchedulerMappings.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Mappings/AssignSchedulerMappings.cs
@@ -11,20 +11,15 @@ namespace PerformanceAssessmentApi.Mappings
         {
             CreateMap<AssignSchedulerCreationDto, AssignScheduler>(MemberList.None)
                 .ForMember(dto => dto.AssessmentId, opt => opt.MapFrom(st => (st.AssessmentId!)))
-                .ForMember(dto => dto.Reminder, opt => opt.MapFrom(st => (st.Reminder!)))
-                .ForMember(dto => dto.Occurrence, opt => opt.MapFrom(st => (st.Occurrence!)))
                 .ForMember(dto => dto.DueDate, opt => opt.MapFrom(st => st.DueDate))
                 .ForMember(dto => dto.Time, opt => opt.MapFrom(st => st.Time))
-                .ForMember(dto => dto.Score, opt => opt.MapFrom(st => st.Score))
                 .ForMember(dto => dto.DateTimeCreated, opt => opt.MapFrom(st => StringUtil.GetCurrentDateTime()))
                 .ForMember(dto => dto.DateTimeUpdated, opt => opt.MapFrom(st => StringUtil.GetCurrentDateTime()));
 
             CreateMap<AssignSchedulerUpdationDto, AssignScheduler>(MemberList.None)
-                .ForMember(dto => dto.Reminder, opt => opt.MapFrom(st => (st.Reminder!)))
-                .ForMember(dto => dto.Occurrence, opt => opt.MapFrom(st => (st.Occurrence!)))
+                .ForMember(dto => dto.IsAnswered, opt => opt.MapFrom(st => (st.IsAnswered!)))
                 .ForMember(dto => dto.DueDate, opt => opt.MapFrom(st => st.DueDate))
                 .ForMember(dto => dto.Time, opt => opt.MapFrom(st => st.Time))
-                .ForMember(dto => dto.Score, opt => opt.MapFrom(st => st.Score))
                 .ForMember(dto => dto.DateTimeUpdated, opt => opt.MapFrom(st => StringUtil.GetCurrentDateTime()));
         }
     }
