8d20a8007047f2fb33b0487c18f78c1f48683d4f
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-16 23:01:36 +0800

    Modify AssignSchedulerDto

--- PerformanceAssessment/PerformanceAssessmentApi/Dtos/AssignSchedulerDto.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/AssignSchedulerDto.cs
@@ -5,11 +5,9 @@
         public int Id { get; set; }
         public int AssessmentId { get; set; }
         public int EmployeeId { get; set; }
-        public string? Reminder { get; set; }
-        public string? Occurrence { get; set; }
+        public bool IsAnswered { get; set; }
         public string? DueDate { get; set; }
         public string? Time { get; set; }
-        public float Score { get; set; }
         public string? DateTimeCreated { get; set; }
         public string? DateTimeUpdated { get; set; }
     }
