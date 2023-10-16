8f31e13d5b3e1cb1fe1bf63e79f16ed4a0f2aa29
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-16 23:02:03 +0800

    Add ResultCreationDto

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/ResultCreationDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PerformanceAssessmentApi.Dtos
+{
+    public class ResultCreationDto
+    {
+        [Required(ErrorMessage = "The assessmentId is required.")]
+        public int AssessmentId { get; set; }
+
+        [Required(ErrorMessage = "The employeeId is required.")]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "The dueDate is required.")]
+        public string? DateTimeDue { get; set; }
+    }
+}
