d70cae3972035ad5521577b93640ba5805155b97
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-17 19:07:29 +0800

    Implement GenerateJwtToken Method in ITokenService

--- PerformanceAssessment/PerformanceAssessmentApi/Services/ITokenService.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Services/ITokenService.cs
@@ -5,6 +5,7 @@ namespace PerformanceAssessmentApi.Services
     public interface ITokenService
     {
         string CreateToken(Employee employee);
+        string GenerateJwtToken(int userId);
         RefreshToken GenerateRefreshToken();
         string VerifyToken(string refreshToken, Employee employee);
     }
