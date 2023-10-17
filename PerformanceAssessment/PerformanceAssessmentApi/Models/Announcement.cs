a61e365b525646baa77022d3d3b470b0249d7be6
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-17 18:30:29 +0800

    Add Necessary Models for Notifications

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Models/Announcement.cs
@@ -0,0 +1,10 @@
+﻿namespace PerformanceAssessmentApi.Models
+{
+    public class Announcement
+    {
+        public int Id { get; set; }
+        public string? Content { get; set; }
+        public string? DateTimeCreated { get; set; }
+        public string? DateTimeUpdated { get; set; }
+    }
+}
