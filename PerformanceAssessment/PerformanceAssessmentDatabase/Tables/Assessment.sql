429d4af2600153d713d3542d74015fde52fb175c
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-18 18:15:24 +0800

    Add EmployeeId Field in Assessment Table

--- PerformanceAssessment/PerformanceAssessmentDatabase/Tables/Assessment.sql
+++ PerformanceAssessment/PerformanceAssessmentDatabase/Tables/Assessment.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[Assessment]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [EmployeeId] INT NOT NULL,
     [Title] NVARCHAR(MAX) NOT NULL, 
     [Description] NVARCHAR(MAX) NOT NULL,
     [DateTimeCreated] DATETIME NOT NULL,
-    [DateTimeUpdated] DATETIME NOT NULL
+    [DateTimeUpdated] DATETIME NOT NULL,
+    CONSTRAINT [FK_AssessmentEmployee] FOREIGN KEY ([EmployeeId]) REFERENCES [Employee]([Id])
 )
