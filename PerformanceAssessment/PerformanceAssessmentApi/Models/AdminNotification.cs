519c4edb778eaa3f91465fcbe64067915bda3d73
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-18 21:07:55 +0800

    Add New Fields in AdminNotification Model

--- PerformanceAssessment/PerformanceAssessmentApi/Models/AdminNotification.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Models/AdminNotification.cs
@@ -4,7 +4,9 @@
     {
         public int Id { get; set; }
         public int EmployeeId { get; set; }
-        public int AssessmentId { get; set; }
+        public string? EmployeeName { get; set; }
+        public string? AssessmentTitle { get; set; }
+        public string? TeamName { get; set; }
         public string? DateTimeCreated { get; set; }
     }
 }
