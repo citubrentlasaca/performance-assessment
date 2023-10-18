01f9c4edbcf25bf2a4f7c83ab3d412439c166959
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2023-10-18 16:52:59 +0800

    Fix routing

--- performance-assessment-application/src/Shared/TopBarTwo.js
+++ performance-assessment-application/src/Shared/TopBarTwo.js
@@ -1,9 +1,11 @@
 import { Box, Stack } from '@mui/material'
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function TopBarTwo() {
     const navigate = useNavigate();
+    const location = useLocation();
+    const organizationId = location.pathname.split('/')[2];
 
     const navigateToOrganizations = () => {
         localStorage.removeItem('employeeData');
@@ -33,7 +35,8 @@ function TopBarTwo() {
             >
                 <ul class="nav h-100 w-100 d-flex justify-content-center align-items-center">
                     <li class="nav-item h-100 col d-flex justify-content-center align-items-center">
-                        <a class="nav-link h-100 w-100 p-0" href="#"
+                        <a class="nav-link h-100 w-100 p-0"
+                            href={`/organizations/${organizationId}`}
                             style={{
                                 color: '#055c9d',
                             }}
@@ -58,13 +61,14 @@ function TopBarTwo() {
                                     <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
                                         <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
                                     </svg>
-                                    <b>Company A</b>
+                                    <b>General</b>
                                 </Stack>
                             </Box>
                         </a>
                     </li>
                     <li class="nav-item h-100 col d-flex justify-content-center align-items-center">
-                        <a class="nav-link h-100 w-100 p-0 d-flex justify-content-center align-items-center" href="/organizations/employees"
+                        <a class="nav-link h-100 w-100 p-0 d-flex justify-content-center align-items-center"
+                            href={`/organizations/${organizationId}/employees`}
                             style={{
                                 color: 'white',
                             }}
@@ -73,7 +77,8 @@ function TopBarTwo() {
                         </a>
                     </li>
                     <li class="nav-item h-100 col d-flex justify-content-center align-items-center">
-                        <a class="nav-link h-100 w-100 p-0 d-flex justify-content-center align-items-center" href="/organizations/adminassessments"
+                        <a class="nav-link h-100 w-100 p-0 d-flex justify-content-center align-items-center" 
+                            href={`/organizations/${organizationId}/admin-assessments`}
                             style={{
                                 color: 'white',
                             }}
@@ -82,7 +87,8 @@ function TopBarTwo() {
                         </a>
                     </li>
                     <li class="nav-item h-100 col d-flex justify-content-center align-items-center">
-                        <a class="nav-link h-100 w-100 p-0 d-flex justify-content-center align-items-center" href="#"
+                        <a class="nav-link h-100 w-100 p-0 d-flex justify-content-center align-items-center" 
+                            href={`/organizations/${organizationId}/admin-analytics`}
                             style={{
                                 color: 'white',
                             }}
