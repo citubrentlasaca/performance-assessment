4b5dfa45ca14a19810293f1f572b9de601320e81
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-20 06:49:44 +0800

    Implement GetAssessmentAnswersByEmployeeAndAssessmentId in AnswerRepository

--- PerformanceAssessment/PerformanceAssessmentApi/Repositories/IAnswerRepository.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Repositories/IAnswerRepository.cs
@@ -10,5 +10,6 @@ namespace PerformanceAssessmentApi.Repositories
         Task<AnswerDto> GetAnswersById(int id);
         Task<int> UpdateAnswers(Answer answer);
         Task<int> DeleteAnswers(int id);
+        Task<AssessmentAnswersDto?> GetAssessmentAnswersByEmployeeAndAssessmentId(int employeeId, int assessmentId);
     }
 }
