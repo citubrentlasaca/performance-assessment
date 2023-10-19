8280ac7c4ab371ccc77b81b0a608489f01e0b18c
Ginalyn Caneda <ginalyn.caneda@cit.edu>
2023-10-20 07:53:41 +0800

    Fix announcement page alignment

--- performance-assessment-application/src/OrganizationsPage/OrganizationDetails.js
+++ performance-assessment-application/src/OrganizationsPage/OrganizationDetails.js
@@ -1,43 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../Shared/NavBar";
 import TopBarThree from "../Shared/TopBarThree";
 import TopBarTwo from "../Shared/TopBarTwo";
 import announcementPhoto from './Images/announcement.png';
+import { Stack } from '@mui/material';
 
 function OrganizationDetails() {
     const employee = JSON.parse(localStorage.getItem("employeeData"));
+    const [team, setTeam] = useState({
+        id: null,
+        organization: "",
+        businessType: "",
+        businessAddress: "",
+        teamCode: "",
+        dateTimeCreated: "",
+        dateTimeUpdated: ""
+    });
+
+    useEffect(() => {
+        const fetchTeamDetails = async () => {
+            try {
+                if (employee.teamId) {
+                    const response = await fetch(`https://localhost:7236/api/teams/${employee.teamId}`);
+                    if (response.ok) {
+                        const teamData = await response.json();
+                        setTeam(teamData);
+                    }
+                }
+            } catch (error) {
+                console.error("Error fetching team details:", error);
+            }
+        };
+
+        fetchTeamDetails();
+    }, []);
 
     return (
         <NavBar>
             {employee.role === "Admin" ? <TopBarTwo /> : <TopBarThree />}
-            <div className="announcement-page" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%" }}>
-                <div className="announcement-content" style={{ textAlign: "center" }}>
-                    <h1>Welcome to Company A!</h1>
-                    <img
-                        src={announcementPhoto}
-                        style={{
-                            width: "150px",
-                            height: "150px",
-                        }}
-                        alt="Announcement"
-                    />
-                    <p>No announcement yet</p>
-                </div>
-
-                <div className="add-announcement-button" style={{ position: "absolute", bottom: "0", margin: "10px" }}>
-                    <button style={{
-                        borderRadius: "25px",
-                        border: "1px solid rgba(39, 198, 217, 0.90)",
-                        background: "linear-gradient(254deg, #0273FF 40.22%, #00C0FE 76.98%, #00C0FE 88.25%)",
-                        color: "#fff",
-                        padding: "10px 20px",
-                        fontSize: "16px",
-                        cursor: "pointer"
-                    }}>
-                        Add Announcement
-                    </button>
-                </div>
-            </div>
+            <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                sx={{
+                    position: "fixed",
+                    top: "50%",
+                    left: "40%"
+                }}
+            >
+                <h1 style={{color: "#055C9D", textAlign: "center", justifyContent: "center"}}>Welcome to {team.organization}!</h1>
+                <img
+                    src={announcementPhoto}
+                    style={{
+                        width: "150px",
+                        height: "150px",
+                    }}
+                    alt="Announcement"
+                />
+                <p style={{textAlign: "center"}}>No announcement yet</p>
+            </Stack>
+            <Stack 
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="center"
+                sx={{ position: "fixed", bottom: "0", margin: "25px 30px 25px 30px"}}
+            >
+                <button style={{
+                    borderRadius: "10px",
+                    border: "1px solid rgba(39, 198, 217, 0.90)",
+                    background: "linear-gradient(254deg, #0273FF 40.22%, #00C0FE 76.98%, #00C0FE 88.25%)",
+                    color: "#fff",
+                    padding: "10px 20px",
+                    fontSize: "16px",
+                    cursor: "pointer",
+                }}>
+                    Add Announcement
+                </button>
+            </Stack>
         </NavBar>
     );
 }
