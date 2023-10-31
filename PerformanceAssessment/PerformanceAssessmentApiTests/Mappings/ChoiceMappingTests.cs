ab578c5f9369be333c4bfd1dc09b295f7858729d
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-10-31 21:24:00 +0800

    Add ChoiceMappingTests

---
+++ PerformanceAssessment/PerformanceAssessmentApiTests/Mappings/ChoiceMappingTests.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using PerformanceAssessmentApi.Dtos;
+using PerformanceAssessmentApi.Mappings;
+using PerformanceAssessmentApi.Models;
+
+namespace PerformanceAssessmentApiTests.Mappings
+{
+    public class ChoiceMappingTests
+    {
+        private readonly IMapper _mapper;
+
+        public ChoiceMappingTests()
+        {
+            var mappingConfig = new MapperConfiguration(cfg => cfg.AddProfile(new ChoiceMappings()));
+            mappingConfig.AssertConfigurationIsValid();
+            _mapper = mappingConfig.CreateMapper();
+        }
+
+        [Fact]
+        public void Map_ValidChoiceCreationDto_ReturnsChoice()
+        {
+            // Arrange
+            var choiceCreationDto = new ChoiceCreationDto
+            {
+                ChoiceValue = "Evaluation",
+                Weight = 25,
+                ItemId = 1,
+            };
+
+            // Act
+            var choice = _mapper.Map<Choice>(choiceCreationDto);
+
+            // Assert
+            Assert.Equal(choiceCreationDto.ChoiceValue, choice.ChoiceValue);
+            Assert.Equal(choiceCreationDto.Weight, choice.Weight);
+            Assert.Equal(choiceCreationDto.ItemId, choice.ItemId);
+        }
+
+        [Fact]
+        public void Map_ValidChoiceUpdationDto_ReturnsChoice()
+        {
+            // Arrange
+            var choiceUpdationDto = new ChoiceUpdationDto
+            {
+                ChoiceValue = "Analytics",
+                Weight = 50,
+            };
+
+            // Act
+            var choice = _mapper.Map<Choice>(choiceUpdationDto);
+
+            // Assert
+            Assert.Equal(choiceUpdationDto.ChoiceValue, choice.ChoiceValue);
+            Assert.Equal(choiceUpdationDto.Weight, choice.Weight);
+        }
+    }
+}
