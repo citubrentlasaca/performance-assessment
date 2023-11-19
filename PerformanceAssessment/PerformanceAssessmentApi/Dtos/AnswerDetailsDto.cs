7078ee733a9dbc0332d77f535d0e5832cfacb5e4
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2023-11-19 19:00:18 +0800

    Add ResultId in AnswerDtos

---
+++ PerformanceAssessment/PerformanceAssessmentApi/Dtos/AnswerDetailsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace PerformanceAssessmentApi.Dtos
+{
+    public class AnswerDetailsDto
+    {
+        public List<int> ResultIds { get; set; }
+        public AnswerCreationDto Answer { get; set; }
+    }
+}
