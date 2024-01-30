81177505a18756286f09d030c85b6bf3d6b5ab6a
Jonas Angelo Clamor <jonasangelo.clamor@cit.edu>
2024-01-30 16:22:01 +0800

    Add IsRead Field In All Notification Models

--- PerformanceAssessment/PerformanceAssessmentApi/Models/AdminNotification.cs
+++ PerformanceAssessment/PerformanceAssessmentApi/Models/AdminNotification.cs
@@ -7,6 +7,7 @@
         public string? EmployeeName { get; set; }
         public string? AssessmentTitle { get; set; }
         public string? TeamName { get; set; }
+        public bool IsRead { get; set; }
         public string? DateTimeCreated { get; set; }
     }
 }
